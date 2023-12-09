9047ad185bb78b4815150b2cb281f1953fee1392
Jade Fung <jade@fungj.net>
2023-12-08 21:19:24 -0800

    made a title

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { CssVarsProvider, Sheet } from "@mui/joy";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <CssVarsProvider>
+      <Sheet>
+        <App />
+      </Sheet>
+    </CssVarsProvider>
+  </React.StrictMode>
+);

71d1afd3d816895800cc8640db444b277dbbf447
Jade Fung <jade@fungj.net>
2023-12-08 21:28:54 -0800

    made scanner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
-import { Grid, Typography } from "@mui/joy";
+import { Button, Grid, Typography } from "@mui/joy";
+import { useState } from "react";
+import Scanner from "./components/Scanner";
 
+type AppState = "idle" | "scanning";
 function App() {
+  const [appState, setAppState] = useState<AppState>("scanning");
+
   return (
-    <Grid container>
-      <Typography
-        level="h1"
-        fontWeight={"bold"}
-        width={"100%"}
-        textAlign={"center"}
-      >
-        Textiles Screen Library
-      </Typography>
+    <Grid
+      container
+      width={"100vw"}
+      minHeight={"100vh"}
+      justifyContent={"center"}
+      alignContent={"center"}
+    >
+      <Grid container justifyContent={"center"}>
+        <Typography
+          level="h1"
+          fontWeight={"bold"}
+          width={"100%"}
+          textAlign={"center"}
+          marginBottom={".5rem"}
+        >
+          Textiles Screen Library
+        </Typography>
+        {appState === "idle" && (
+          <Button color="primary">
+            <Typography level="h1" sx={{ color: "#FFF" }}>
+              Touch to start
+            </Typography>
+          </Button>
+        )}
+        {appState === "scanning" && <Scanner />}
+      </Grid>
     </Grid>
   );
 }
---
+++ src/components/Scanner/index.tsx
@@ -0,0 +1,5 @@
+const Scanner = () => {
+  return <div>SCANNER!</div>;
+};
+
+export default Scanner;
